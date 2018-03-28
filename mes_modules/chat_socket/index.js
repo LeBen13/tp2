2c2106227bb4e229beaea475bc705d934d47f2aa
Unknown <Benjymarinier@gmail.com>
2018-03-27 20:26:20 -0400

    Modules commits

---
+++ mes_modules/chat_socket/index.js
@@ -0,0 +1,29 @@
+let socketio = require('socket.io')
+
+module.exports.listen = function (server) {
+    let io = socketio.listen(server)
+
+    // ------------------------------ Traitement du socket
+    let objUtilisateur = {}
+    io.on('connection', function (socket) {
+        TRAITEMENT DE LA CONNECTION DES USERS
+        socket.on('setUser', function (data) {
+            objUtilisateur[socket.id] = data.user;
+            socket.emit('valide_user', data);
+            io.sockets.emit('diffuser_liste_user', objUtilisateur);
+        })
+
+        socket.on('setMessage', function (data) {
+            data.user = objUtilisateur[socket.id];
+            socket.broadcast.emit('diffuserMessage', data);
+            socket.emit('valideMessage', data);
+        })
+
+        socket.on('disconnect', function () {
+            delete objUtilisateur[socket.id];
+            io.sockets.emit('diffuser_destruction_user', objUtilisateur);
+
+        })
+    })
+    return io
+}
